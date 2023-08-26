b49b97b3ebef50daf4fcc5f46bb4119622e41be4
Matheus Berger <mcoelhoberger@apple.com>
2023-08-25 21:03:48 -0300

    getting pedal name and brand from user input

--- pedalboard-web/src/App.vue
+++ pedalboard-web/src/App.vue
@@ -1,38 +1,27 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 import Home from './components/Home.vue'
 </script>
 
 <template>
-  <Home/>
+  <div>
+    <header>
+      <h1>Welcome to PedalBoard!</h1>
+    </header>
+    <div class="content"> 
+      <Home/>
+    </div>
+  </div>
 </template>
 
-<style scoped>
+<style>
 header {
-  line-height: 1.5;
-}
+  display: flex;
+  flex:10%;
+};
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.content {
+  display: flex;
+  flex-direction: column;
+  flex: 80%;
 }
 </style>
--- pedalboard-web/src/components/Home.vue
+++ pedalboard-web/src/components/Home.vue
@@ -1,11 +1,53 @@
 <script setup lang="ts">
 import { ref, reactive } from 'vue'
+import type { Ref } from 'vue'
+import { type Pedal } from '../models/pedal'
+import { type Knob } from '../models/knob'
+
+const pedals: Ref<Pedal[]> = ref([])
+
+const pedalName = ref('')
+const pedalBrand = ref('')
+const pedalKnobs = ref([])
+
+const addPedal = () => {
+    if (pedalName.value != '' && pedalBrand.value != '') {
+        pedals.value.push({ name: pedalName.value, brand: pedalBrand.value, knobs: pedalKnobs.value })
+
+        pedalName.value = ''
+        pedalBrand.value = ''
+        pedalKnobs.value = []
+    }
+    console.log(pedals.value)
+}
 </script>
 
 <template>
-<h1>Welcome to PedalBoard</h1>
+  <form @submit.prevent="addPedal">
+    <input type="text" v-model="pedalName" placeholder="name your pedal">
+    <input type="text" v-model="pedalBrand" placeholder="pedal's brand">
+    <button>create pedal</button>
+  </form>
 </template>
 
 <style>
+form {
+    display: flex;
+    flex-direction: column;
+    width: 40%;
+}
 
+input {
+    margin: 10px;
+    padding: 10px;
+    border-radius: 10px;
+}
+
+button {
+    margin: 10px;
+    padding: 5px;
+    font-weight: bold;
+    font-size: large;
+    border-radius: 10px;
+}
 </style>
