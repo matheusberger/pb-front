ba7e70336da90abe679495334d7d49ee716381a2
Matheus Berger <mcoelhoberger@apple.com>
2023-08-25 21:40:02 -0300

    getting multiple knobs from user

--- pedalboard-web/src/components/Home.vue
+++ pedalboard-web/src/components/Home.vue
@@ -6,26 +6,51 @@ import { type Knob } from '../models/knob'
 
 const pedals: Ref<Pedal[]> = ref([])
 
+const knobCount = ref(1);
 const pedalName = ref('')
 const pedalBrand = ref('')
-const pedalKnobs = ref([])
+const pedalKnobs: Ref<Knob[]> = ref([{ id: knobCount.value, name: '', value: 0 }])
 
 const addPedal = () => {
     if (pedalName.value != '' && pedalBrand.value != '') {
         pedals.value.push({ name: pedalName.value, brand: pedalBrand.value, knobs: pedalKnobs.value })
 
+        knobCount.value = 1
         pedalName.value = ''
         pedalBrand.value = ''
-        pedalKnobs.value = []
+        pedalKnobs.value = [{ id: knobCount.value, name: '', value: 0 }]
     }
     console.log(pedals.value)
+}
+
+const addKnob = () => {
+    knobCount.value++
+    pedalKnobs.value.push({ id: knobCount.value, name:'', value: 0})
+}
+
+const removeKnob = (knob: Knob) => {
+    pedalKnobs.value = pedalKnobs.value.filter((k) => k !== knob)
 }
 </script>
 
 <template>
   <form @submit.prevent="addPedal">
-    <input type="text" v-model="pedalName" placeholder="name your pedal">
-    <input type="text" v-model="pedalBrand" placeholder="pedal's brand">
+    <div class="singleInput">
+        <h2>Name</h2>
+        <input v-model="pedalName" placeholder="name your pedal">
+    </div>
+    <div class="singleInput">
+        <h2>Brand</h2>
+        <input v-model="pedalBrand" placeholder="pedal's brand">
+    </div>
+    <div class="singleInput">
+        <h2>Knobs</h2>
+        <button @click="addKnob" type="button">+</button>
+    </div>
+    <div class="singleInput" v-for="knob in pedalKnobs" :key="knob.id">
+        <input v-model="knob.name" placeholder="knob name here">
+        <button @click="" type="button">x</button>
+    </div>
     <button>create pedal</button>
   </form>
 </template>
@@ -35,6 +60,12 @@ form {
     display: flex;
     flex-direction: column;
     width: 40%;
+}
+
+.singleInput {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
 }
 
 input {
--- pedalboard-web/src/models/knob.ts
+++ pedalboard-web/src/models/knob.ts
@@ -1,4 +1,5 @@
 export interface Knob {
+    id: number;
     name: string;
     value: number;
 }
